cd921b1d8b9cd8573b9836481b5c51039a888999
PQrux <guineub753@gmail.com>
2019-02-24 00:10:13 -0300

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\gui\material de estudo\PYTHON SCRIPT\PyProjects\TextFightGame\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
